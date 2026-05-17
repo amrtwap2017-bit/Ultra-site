0978cf03eabf04a4c70e65f9fd6adcd857803ab3
amrtwap2017-bit <amr.twap2017@gmail.com>
2026-05-18 02:16:24 +0300

    vc

--- src/declarations.d.ts
+++ src/declarations.d.ts
@@ -1,16 +1,19 @@
-declare module '*.png' {
-  const content: string;
-  export default content;
+declare module "*.png" {
+  const src: string;
+  export default src;
 }
-declare module '*.jpg' {
-  const content: string;
-  export default content;
+
+declare module "*.jpg" {
+  const src: string;
+  export default src;
 }
-declare module '*.svg' {
-  const content: string;
-  export default content;
+
+declare module "*.jpeg" {
+  const src: string;
+  export default src;
 }
-declare module '*.webp' {
-  const content: string;
-  export default content;
+
+declare module "*.webp" {
+  const src: string;
+  export default src;
 }
